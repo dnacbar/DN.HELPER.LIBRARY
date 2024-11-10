a384c2cc06f6b51dd51ec23ad9c00f9c2df9de80
DOUGLAS NACBAR <NACBAR@WASATAIN-PC>
2024-11-10 11:53:14 -0300

    ADICIONA OBJECT EXTENSION

---
+++ SRC/DN.HELPER.LIBRARY.TEST/EXTENSION/ObjectExtensionTest.cs
@@ -0,0 +1,23 @@
+﻿using DN.HELPER.LIBRARY.EXTENSION;
+
+namespace DN.HELPER.LIBRARY.TEST.EXTENSION;
+
+public class ObjectExtensionTest
+{
+    [Fact]
+    public void ValidaSeObjetoNuloRetornaTextoVazio()
+    {
+        string? texto = null;
+
+        Assert.True(texto.ToStringOrEmpty() == string.Empty);
+    }
+
+    [Fact]
+    public void ValidaSeObjetoTextoRetornaTextoIgual()
+    {
+        const string TEXTO = "TEXTO";
+        object texto = TEXTO;
+
+        Assert.True(texto.ToStringOrEmpty() == TEXTO);
+    }
+}
---
+++ SRC/DN.HELPER.LIBRARY/EXTENSION/ObjectExtension.cs
@@ -0,0 +1,6 @@
+﻿namespace DN.HELPER.LIBRARY.EXTENSION;
+
+public static class ObjectExtension
+{
+    public static string ToStringOrEmpty(this object objString) => objString == null ? string.Empty : objString.ToString();
+}
